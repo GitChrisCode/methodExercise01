f75fd78f67a6b5ff366691a4309751b7bc7bdc30
GitChrisCode <chrha.gw2@gmail.com>
2023-04-18 12:24:45 +0200

    initial commit + sum function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(sum(5, 10));
     }
 
     public static int sum(int a, int b) {
